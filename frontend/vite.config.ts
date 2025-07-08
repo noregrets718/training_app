cabe9a24bfd36b83f790acb74da7ec98d2768ca0
user <user@MacBook-Air-user.local>
2025-07-08 22:13:22 +0700

    df

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
     hmr: {
       protocol: 'wss',
       host: 'trainingapp.ru',
-      path: '/ws', // путь WebSocket (по умолчанию /, но можно указать явно)     // это то, что слушает Vite внутри
+      clientPort: 443
+      // path: '/ws', // путь WebSocket (по умолчанию /, но можно указать явно)     // это то, что слушает Vite внутри
     },
   },
 })
